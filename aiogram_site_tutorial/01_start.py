94a819c79af6b21cb9b340687486131334879565
evgeniy <pavlinbl4@gmail.com>
2024-05-11 20:11:17 +0300

    tutorial bot with filter by user id

--- aiogram_site_tutorial/01_start.py
+++ aiogram_site_tutorial/01_start.py
@@ -6,6 +6,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
 from get_credentials import Credentials
@@ -15,29 +16,27 @@ TOKEN = Credentials().pavlinbl4_bot
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
-# Initialize Bot instance with a default parse mode which will be passed to all API calls
-bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 # Define a list of allowed user IDs
-allowed_user_ids: set[int] = {123456789, 987654321, 1237220337, 187597961}
-"""
-This handler receives messages with `/start` command
-"""
+# allowed_user_ids = {123456789, 987654321, 1237220337}
 
 
 @dp.message(CommandStart())
-async def command_start_handler() -> None:
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    # await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    # await message.answer(f"Your id, {hbold(message.from_user.id)}!")
 
 
-@dp.message(F.from_user.id.in_(allowed_user_ids))
+@dp.message(F.from_user.id.in_({187597961}))
 async def handle_allowed_user_messages(message: types.Message):
-    photo_id = message.photo[-1].file_id
-
-    file = await bot.get_file(photo_id)
-    file_path = file.file_path
-
-    await bot.download_file(file_path, f"../DownloadedFiles/{photo_id}.jpg")
-
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}\n"
                          f"your id - {message.from_user.id}\n"
                          f"your username - {message.from_user.username}\n"
@@ -48,22 +47,12 @@ async def handle_allowed_user_messages(message: types.Message):
                          f"your is bot - {message.from_user.is_bot}\n"
                          f"your url - {message.from_user.url}\n"
                          f"your language code - {message.from_user.language_code}\n"
-                         f"your model config - {message.from_user.model_config}\n"
-                         f"photo_id - {photo_id}\n"
-                         f"file - {file_path}\n"
                          f"you are allowed user!")
 
     """
     Handler will forward receive a message back to the sender
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
-    # try:
-    #     # Send a copy of the received message
-    #     await message.reply("Hello, allowed user!")
-    #     await message.send_copy(chat_id=message.chat.id)
-    # except TypeError:
-    #     # But not all the types is supported to be copied so need to handle it
-    #     await message.answer("Nice try!")
 
 
 @dp.message()
@@ -73,6 +62,8 @@ async def handle_other_messages(message: types.Message):
 
 
 async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     # And the run events dispatching
     await dp.start_polling(bot)
 
